b42214da6e7ed7d2099bea2cbc3e4759f4b41645
unknown <m.bellone@yahoo.com>
2025-10-07 13:55:10 -0300

    Documentacion de las clases Paciente y Persona

--- src/main/java/modelo/personas/Persona.java
+++ src/main/java/modelo/personas/Persona.java
@@ -2,6 +2,10 @@ package modelo.personas;
 
 import util.Domicilio;
 
+/**
+ * Clasee abastracta Persona que representa a una persona
+ * Contiene atributos nombre, apellido, dni, domicilio y telefono
+ */
 public abstract class Persona {
     private String nombre;
     private String apellido;
@@ -9,6 +13,16 @@ public abstract class Persona {
     private Domicilio domicilio;
     private String telefono;
 
+    /**
+     * Constructor de Persona
+     * <b>pre:</b> nombre, apellido, dni, domicilio y telefono no deben ser nulos ni vacios.
+     * <b>post:</b> se crea una persona con los atributos proporcionados.
+     * @param nombre nombre de la persona
+     * @param apellido apellido de la persona
+     * @param dni dni de la persona
+     * @param domicilio domicilio de la persona
+     * @param telefono telefono de la persona
+     */
     public Persona(String nombre, String apellido, String dni, Domicilio domicilio, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -17,42 +31,80 @@ public abstract class Persona {
         this.telefono = telefono;
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el nombre de la persona.
+     * @return nombre de la persona
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo publico void para setear el nombre de la persona.
+     * <b>pre:</b> nombre no debe ser nulo ni vacio.
+     * <b>post:</b> se setea el nombre de la persona.
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el apellido de la persona.
+     * @return apellido de la persona
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Metodo publico void para setear el apellido de la persona.
+     * <b>pre:</b> apellido no debe ser nulo ni vacio.
+     * <b>post:</b> se setea el apellido de la persona.
+     * @param apellido
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
+    /**
+     * Metodo publico String para preguntar cual es el dni de la persona.
+     * @return dni de la persona
+     */
     public String getDni() {
         return dni;
     }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
+    /**
+     * Metodo publico Domicilio para preguntar cual es el domicilio de la persona.
+     * @return domicilio de la persona
+     */
     public Domicilio getDomicilio() {
         return domicilio;
     }
 
+    /**
+     * Metodo publico void para setear el domicilio de la persona.
+     * <b>pre:</b> domicilio no debe ser nulo.
+     * <b>post:</b> se setea el domicilio de la persona.
+     * @param domicilio
+     */
     public void setDomicilio(Domicilio domicilio) {
         this.domicilio = domicilio;
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el telefono de la persona.
+     * @return telefono de la persona
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Metodo publico void para setear el telefono de la persona.
+     * <b>pre:</b> telefono no debe ser nulo ni vacio.
+     * <b>post:</b> se setea el telefono de la persona.
+     * @param telefono
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
--- src/main/java/modelo/personas/paciente/PacienteFactory.java
+++ src/main/java/modelo/personas/paciente/PacienteFactory.java
@@ -4,8 +4,25 @@ import util.Domicilio;
 
 import java.time.LocalDate;
 
+/**
+ * Clase PacienteFactory que crea pacientes segun su edad
+ */
 public class PacienteFactory
 {
+    /**
+     * Metodo que crea un paciente segun su edad
+     * <b>pre:</b> edad debe ser mayor o igual a 0
+     * <b>post:</b> se retorna una instancia de Paciente correspondiente a la edad
+     * @param dni
+     * @param nombre
+     * @param apellido
+     * @param domicilio
+     * @param telefono
+     * @param nroHistoriaMedica
+     * @param edad
+     * @param fechaIngreso
+     * @return Paciente (PacienteNino, PacienteJoven o PacienteMayor)
+     */
     public Paciente crearPaciente(String dni, String nombre, String apellido, Domicilio domicilio, String telefono, String nroHistoriaMedica, int edad, LocalDate fechaIngreso)
     {
         if (edad < 15)
