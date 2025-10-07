87166aa711ee2df21d21bce5167e937538e0a221
unknown <m.bellone@yahoo.com>
2025-10-07 12:45:32 -0300

    Finalizacion de documentacion de las excepciones

--- src/main/java/util/Excepciones/ContratacionNoExistenteException.java
+++ src/main/java/util/Excepciones/ContratacionNoExistenteException.java
@@ -1,6 +1,15 @@
 package util.Excepciones;
-
-public class ContratacionNoExistenteException extends Exception {
+/**
+ * Excepción lanzada cuando se intenta acceder a una contratación que no existe.
+ */
+public class ContratacionNoExistenteException extends Exception
+{
+    /**
+     * Constructor de la excepción.
+     * <b>pre:</b> message != null
+     * <b>post:</b> Crea una nueva excepción con el mensaje proporcionado
+      * @param message Mensaje de error.
+     */
     public ContratacionNoExistenteException(String message) {
         super(message);
     }
