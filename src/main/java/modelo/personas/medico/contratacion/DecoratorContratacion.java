02ecc969dbc88ebff6cc1ed2e22160fea5699e5a
unknown <m.bellone@yahoo.com>
2025-10-07 14:30:52 -0300

    Finalizacion de documentacion de carpeta medico

--- src/main/java/modelo/personas/medico/contratacion/DecoratorContratacion.java
+++ src/main/java/modelo/personas/medico/contratacion/DecoratorContratacion.java
@@ -15,7 +15,6 @@ public abstract class DecoratorContratacion implements IMedico
      * Constructor de DecoratorContratacion
      * <b>pre:</b> medico no debe ser nulo.
      * <b>post:</b> se crea un DecoratorContratacion con el medico proporcionado.
-     *
      * @param medico
      */
     public DecoratorContratacion(DecoratorPosgrado medico)
@@ -23,18 +22,30 @@ public abstract class DecoratorContratacion implements IMedico
         this.encapsulado = medico;
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el numero de matricula del medico.
+     * @return nroMatricula del medico
+     */
     @Override
     public String getNroMatricula()
     {
         return this.encapsulado.getNroMatricula();
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el nombre del medico.
+     * @return nombre del medico
+     */
     @Override
     public String getNombre()
     {
         return this.encapsulado.getNombre();
     }
 
+    /**
+     * Metodo publico String para preguntar cual es el apellido del medico.
+     * @return apellido del medico
+     */
     @Override
     public String getApellido()
     {
