6be9f0c9552315b44164fad08509c6c3470d422d
unknown <m.bellone@yahoo.com>
2025-10-08 14:54:29 -0300

    Documentacion del modulo de ingreso

--- src/main/java/modelo/clinica/modulos/ingreso/SalaDeEspera.java
+++ src/main/java/modelo/clinica/modulos/ingreso/SalaDeEspera.java
@@ -1,15 +1,30 @@
 package modelo.clinica.modulos.ingreso;
 
 import modelo.personas.paciente.Paciente;
-
+/**
+ * Clase SalaDeEspera que representa la sala de espera de la clinica.
+ * Contiene en relacion de composicion el patio y la sala privada.
+ */
 public class SalaDeEspera {
     Patio patio;
     SalaPrivada salaPrivada;
 
+    /**
+     * Constructor de SalaDeEspera
+     * <b>pre:</b> patio y salaPrivada no deben ser nulos.
+     * <b>post:</b> se crea una sala de espera con el patio y la sala privada proporcionados.
+     * @param p Patio de la sala de espera.
+     * @param s Sala privada de la sala de espera.
+     */
     public SalaDeEspera(Patio p, SalaPrivada s){
         patio = p;
         salaPrivada = s;
     }
+
+    /**
+     * Metodo publico Patio para preguntar cual es el patio de la sala de espera.
+     * @return patio de la sala de espera.
+     */
     public void ingresaPaciente(Paciente p){
         Paciente pacienteSalaPrivada = salaPrivada.getPaciente();
         if (!salaPrivada.isOcupado()){
@@ -23,6 +38,14 @@ public class SalaDeEspera {
             }
         }
     }
+
+    /**
+     * Metodo publico boolean para sacar un paciente de la sala de espera.
+     * <b>pre:</b> paciente no debe ser nulo.
+     * <b>post:</b> se saca el paciente de la sala de espera si existe en el patio o en la sala privada.
+     * @param p Paciente a sacar de la sala de espera.
+     * @return true si el paciente fue sacado, false si no estaba en la sala de espera.
+     */
     public boolean sacarPaciente(Paciente p){
          boolean res = this.patio.sacarPaciente(p);
          if (!res){
--- src/main/java/modelo/clinica/modulos/ingreso/SistemaIngreso.java
+++ src/main/java/modelo/clinica/modulos/ingreso/SistemaIngreso.java
@@ -1,18 +1,37 @@
 package modelo.clinica.modulos.ingreso;
 
 import modelo.personas.paciente.Paciente;
-
+/**
+ * Clase SistemaIngreso que representa el sistema de ingreso de pacientes a la clinica.
+ * Contiene en relacion de composicion la sala de espera.
+ */
 public class SistemaIngreso
 {
     private SalaDeEspera salaDeEspera;
-
+    /**
+     * Constructor de SistemaIngreso
+     * <b>post:</b> se crea un sistema de ingreso con una sala de espera.
+     */
     public SistemaIngreso() {
         this.salaDeEspera = new SalaDeEspera(new Patio(), new SalaPrivada());
     }
-
+    /**
+     * Metodo publico void para ingresar un paciente a la sala de espera.
+     * <b>pre:</b> paciente no debe ser nulo.
+     * <b>post:</b> se ingresa el paciente a la sala de espera.
+     * @param p Paciente a ingresar a la sala de espera.
+     */
     public void ingresaPaciente(Paciente p){
         salaDeEspera.ingresaPaciente(p);
     }
+
+    /**
+     * Metodo publico boolean para sacar un paciente de la sala de espera.
+     * <b>pre:</b> paciente no debe ser nulo.
+     * <b>post:</b> se saca el paciente de la sala de espera si existe en el patio o en la sala privada.
+     * @param p Paciente a sacar de la sala de espera.
+     * @return true si el paciente fue sacado, false si no estaba en la sala de espera.
+     */
     public boolean sacarPacienteSalaDeEspera(Paciente p){
         return salaDeEspera.sacarPaciente(p);
     }
