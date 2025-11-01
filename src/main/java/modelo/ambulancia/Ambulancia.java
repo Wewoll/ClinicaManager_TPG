066d8c47ac29719868108e94d2b92d957dd108c8
mbell <m.bellone@yahoo.com>
2025-11-01 15:50:19 -0300

    Implementacion de interfaz State y clase Ambulancia

--- src/main/java/modelo/ambulancia/Ambulancia.java
+++ src/main/java/modelo/ambulancia/Ambulancia.java
@@ -1,6 +1,20 @@
 package modelo.ambulancia;
 
 public class Ambulancia {
-    State estadoActual;
+    private State estadoActual;
+
+
+    public Ambulancia() {
+        this.estadoActual = new DisponibleState();
+    }
+
+    public void cambiarEstado(State nuevoEstado) {
+        this.estadoActual = nuevoEstado;
+    }
+
+    public State getEstadoActual() {
+        return this.estadoActual;
+    }
+
 
 }
--- src/main/java/modelo/ambulancia/State.java
+++ src/main/java/modelo/ambulancia/State.java
@@ -1,5 +1,8 @@
 package modelo.ambulancia;
 
 public interface State {
-
+    void SolicitudDeTraslado(Ambulancia ambulancia);
+    void SolicitudDeArencionDomicilio(Ambulancia ambulancia);
+    void SolicitudMantenimiento(Ambulancia ambulancia);
+    void RetornoDeTaller(Ambulancia ambulancia);
 }
